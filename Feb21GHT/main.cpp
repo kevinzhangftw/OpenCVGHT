c1c2937f58c1813a9c04da93bcb2bfdd2dd88c7f
Kai Zhang <kai@d207-023-183-030.wireless.sfu.ca>
2017-02-21 15:54:49 -0800

    Initial Commit

---
+++ Feb21GHT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Feb21GHT
+//
+//  Created by Kai Zhang on 2017-02-21.
+//  Copyright © 2017 Kai Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
